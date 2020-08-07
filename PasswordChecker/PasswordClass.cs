fde8cfc9457b3f24e391fd2bcb6eab129c3d8ee2
Issac Tumuti <issac.tumuti@ad.gov.sc>
2020-08-07 10:54:34 +0400

    Added Generate Password Method

--- PasswordChecker/PasswordClass.cs
+++ PasswordChecker/PasswordClass.cs
@@ -1,10 +1,19 @@
 ﻿using System;
 using System.Linq;
+using System.Text;
 
 namespace PasswordChecker
 {
+    /// <summary>
+    /// A Class that checks and generates password
+    /// </summary>
     public class PasswordClass
     {
+        private string numbers = "0123456789";
+        private string lower_case = "abcdefghijklmnopqrstuvwxyz";
+        private string upper_case = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        private  string special_characters = "!@#$%^&*()-+";
+
         /// <summary>
         /// returns the minimum number of characters required to make the password strong
         /// </summary>
@@ -13,14 +22,8 @@ namespace PasswordChecker
         /// <returns></returns>
         public int CheckPassword(string password,int requiredLength)
         {
-            // Return the minimum number of characters to make the password strong
-            var numbers = "0123456789";
-            var lower_case = "abcdefghijklmnopqrstuvwxyz";
-            var upper_case = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-            var special_characters = "!@#$%^&*()-+";
-            int count = 0;
             int n = password.Length;
-
+            int count = 0;
             bool isNo = false;
             for (int i = 0; i < numbers.Length; i++)
             {
@@ -93,10 +96,7 @@ namespace PasswordChecker
         public bool IsPasswordStrong(string password, int requiredLength)
         {
             // Return the minimum number of characters to make the password strong
-            var numbers = "0123456789";
-            var lower_case = "abcdefghijklmnopqrstuvwxyz";
-            var upper_case = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-            var special_characters = "!@#$%^&*()-+";
+         
             int count = 0;
             int n = password.Length;
 
@@ -165,6 +165,43 @@ namespace PasswordChecker
                 return true;
             }
             return false;
+        }
+    
+
+        /// <summary>
+        /// Generates a random password of the required length
+        /// </summary>
+        /// <param name="passwordLength"></param>
+        /// <returns></returns>
+        public string GeneratePassword(int passwordLength)
+        {
+            Random rnd = new Random();
+            int count = 0;
+            StringBuilder sw = new StringBuilder();
+
+            while (count <= passwordLength)
+            {         
+                int noPosition = rnd.Next(0, numbers.Length);
+                if (count <= passwordLength)
+                   sw.Append(numbers[noPosition]);
+                       
+                int lowerCasePosition = rnd.Next(0,lower_case.Length);
+                if(count <= passwordLength)
+                   sw.Append(lower_case[lowerCasePosition]);
+
+                int upperCasePosition = rnd.Next(0, upper_case.Length);
+                if (count <= passwordLength)
+                    sw.Append(upper_case[upperCasePosition]);
+
+
+                int specialCasePosition = rnd.Next(0, special_characters.Length);
+                if (count <= passwordLength)
+                    sw.Append(special_characters[specialCasePosition]);
+
+                count += 4;
+            }
+
+            return sw.ToString();
         }
     }
 }
---
+++ PasswordChecker/readme.txt
@@ -0,0 +1,27 @@
+﻿# PasswordChecker
+A library that checks if a password is strong by checking if it contains a combination of lowercase,uppercase,special characters and numbers.
+
+
+# Overview
+instructions:
+Install-Package PasswordChecker -Version 1.0.0
+
+
+# Usage
+The Library contains two methods
+
+1.CheckPassword
+
+  -It takes two parameters, the password and required length
+  -The method returns the minimum no of characters required to reach the required length
+  
+2.IsPasswordStrong
+
+ -It takes two parameters, the password and required length
+ -The method returns true if the password has all the required characters and the required length otherwise returns false.
+    
+
+3.GeneratePassword
+  
+   -It takes one parameter,the password length
+   -The method returns a password with a combination of numbers,lowercase,uppercase and special characters of the required length.
